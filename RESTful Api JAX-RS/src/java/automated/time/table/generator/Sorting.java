327b75b517ee20c10d82779f7951d433fbfc52c9
Fares El-Shazly <Fares.ElShazlyy@Gmail.com>
2019-08-06 08:37:09 +0200

    ATTG-Project

--- RESTful Api JAX-RS/src/java/automated/time/table/generator/Sorting.java
+++
@@ -1,9 +0,0 @@
-package automated.time.table.generator;
-import java.util.Comparator;
-
-public class Sorting implements Comparator<Session> {
-    public int compare(Session A, Session B) 
-    { 
-        return A.GetDuration() - B.GetDuration(); 
-    }
-}
